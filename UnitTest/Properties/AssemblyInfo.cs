2d71d0d83216a90c9a70574900065f0f9690c197
weishakeji <10522779@qq.com>
2019-07-30 16:16:41 +0800

    添加单元测试

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d79316f-3553-4b6f-8c20-3d05564e2feb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/TestEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class TestEntry
+    {
+        // Replace with your keys here
+        public string ACCESS_KEY = "QEy_5icAqlfdPL3cr4dLrq2cuuWz78INd57JJeon";
+        public string SECRET_KEY = "oHrKDdeysM9RtZDYdt7HgypCOEuGJrTztSto3uvN";
+
+        // Replace with your hub name
+        public string HUB_NAME = "ceshi5";
+    }
+}
